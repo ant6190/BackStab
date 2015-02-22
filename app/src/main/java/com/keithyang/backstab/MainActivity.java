acc1bc48fd77c5bf551ef930bf589bf96c0dc5f2
ant6190 <anthony.chernyak@gmail.com>
2015-02-22 03:53:27 -0500

    push

--- app/src/main/java/com/keithyang/backstab/MainActivity.java
+++ app/src/main/java/com/keithyang/backstab/MainActivity.java
@@ -8,11 +8,21 @@ import android.location.LocationManager;
 import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URI;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -51,8 +61,22 @@ public class MainActivity extends ActionBarActivity {
 
 
     public void buttonLogin(View view) {
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
+        TelephonyManager mTelephonyMgr;
+        mTelephonyMgr = (TelephonyManager)
+        getSystemService(Context.TELEPHONY_SERVICE);
+        String number = mTelephonyMgr.getLine1Number();
+        try{
+            HttpClient httpclient = new DefaultHttpClient();
+
+            HttpGet request = new HttpGet();
+            URI website = new URI("52.10.137.240:8888/getplayerbyid/" + number);
+            request.setURI(website);
+            HttpResponse response = httpclient.execute(request);
+            BufferedReader in = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+
+        }catch(Exception e){
+            Log.e("log_tag", "Error in http connection "+e.toString());
+        }
     }
 
     public void buttonSignUp(View view) {
